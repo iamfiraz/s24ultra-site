f83fe9db4137f496611779fc7a73af4f531be601
iamfiraz <firazm666@gmail.com>
2025-06-05 12:37:36 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+function calculate() {
+
+
+    var x = parseInt(document.getElementById("num1").value);
+    console.log(x)
+    var y = parseInt(document.getElementById("num2").value);
+    console.log(y)
+    var operator = document.getElementById("op").value;
+    console.log(operator)
+    var result = 0;
+
+    if (operator == "+") {
+        result = x + y;
+    }
+    else if (operator == "-") {
+        result = x - y;
+    }
+    else if (operator == "*") {
+        result = x * y;
+    }
+    else if (operator == "/") {
+        result = x / y;
+    }
+
+    document.getElementById("result-text").innerText = result;
+    event.preventDefault();
+}
